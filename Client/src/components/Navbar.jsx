feed75c6e2f5d11b4673a0e9d9aff1064a94e2e1
Neelesh <130912265+neeleshkr22@users.noreply.github.com>
2024-12-27 20:31:33 +0530

    update

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,45 +1,21 @@
-import React from 'react'
+
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className=' ml-10 mr-10 fixed w-[94vw]'>
-      <div className="navbar bg-base-100">
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-5 w-5 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h16M4 18h16"></path>
-      </svg>
-    </button>
-  </div>
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Zync</a>
-  </div>
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-5 w-5 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-      </svg>
-    </button>
-  </div>
-</div>
-    </div>
-  )
-}
+    <nav className="bg-gray-800 text-white py-4 fixed top-0 left-0 w-full z-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">My App</h1>
+        <div className="space-x-4">
+          <Link to="/" className="hover:text-gray-400">Home</Link>
+          <Link to="/login" className="hover:text-gray-400">Login</Link>
+          <Link to="/register" className="hover:text-gray-400">Register</Link>
+          <Link to="/settings" className="hover:text-gray-400">Theme Settings</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
